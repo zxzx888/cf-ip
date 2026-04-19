62b1c9bded4edf38a5ab824eb9bbf8ba74dc3ecf
zxzx888 <zxzx888@gmail.com>
2026-04-19 22:51:59 +0800

    Update collect_and_test_ips.py

--- collect_and_test_ips.py
+++ collect_and_test_ips.py
@@ -20,7 +20,7 @@ URLS = [
     'https://cf.090227.xyz/ct',
     'https://cf.090227.xyz/CloudFlareYes',
     'https://www.wetest.vip/page/cloudflare/address_v4.html',
-    'https://ipdb.030101.xyz/bestcfv4/',
+    'https://raw.githubusercontent.com/ymyuuu/IPDB/refs/heads/main/BestCF/bestcfv4.txt',
     'https://api.uouin.com/cloudflare.html',
     'https://api.4ce.cn/api/bestCFIP'
 ]
