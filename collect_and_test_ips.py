835a34baf0cd663f9ccc5b63565aa11d49754d0e
zxzx888 <zxzx888@gmail.com>
2026-06-29 00:33:40 +0500

    Remove an unused URL from URLS list
    Removed a URL from the list of IP sources.

--- collect_and_test_ips.py
+++ collect_and_test_ips.py
@@ -18,7 +18,6 @@ TEST_ROUNDS = 6
 URLS = [
     'https://ip.164746.xyz',
     'https://cf.090227.xyz/ct',
-    'https://addressesapi.090227.xyz/CloudFlareYes',
     'https://www.wetest.vip/page/cloudflare/address_v4.html',
     'https://api.uouin.com/cloudflare.html',
     'https://v2rayssr.com/cfip'
